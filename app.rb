c6ccf0449c2db5eab6ce38a853d44727e1f0e785
Itggot-lukas-einlerlarsson <Lukas.einlerlarsson@itggot.se>
2018-03-22 10:47:21 +0100

    fixed vagrant and slim

--- app.rb
+++ app.rb
@@ -1,30 +1,12 @@
-require "Sinatra"
-  set :sessions => true
+class App < Sinatra::Base
 
-  get '/' do
-  slim(:index)
-  end
+	get '/' do
+		"Hello, Grillkorv!"
+		slim(:index)
+	end
 
-	 get '/register' do
-		 slim(:register)
-	 end
+	get '/register' do
+	 slim(:register)
+ end
 
-   post 'login' do
-   session[:user_id] = User.authenticate(params).id
-   end
-
-	 post '/register' do
-		 begin
-				db = SQLite3::Database.new("DTABASAHÄR")
-				username = params[:username]
-				password = params[:password]
-				email = params[:email]
-				password_digest = BCrypt::Password.create(password)
-				db.execute("INSERT INTO Users(username,password_digest,email) VALUES(?,?,?)",[username,password,email])
-			rescue
-				puts "something went wog, try again"
-				redirect('/register')
-			else
-			redirect('/')
-		end
-	 end
+end
