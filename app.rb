8b6e6a62e0f5d0b2d84e2272f62ea660e5b11a9b
Itggot-lukas-einlerlarsson <Lukas.einlerlarsson@itggot.se>
2018-04-12 10:38:30 +0200

    trying to fix login

--- app.rb
+++ app.rb
@@ -1,12 +1,32 @@
 class App < Sinatra::Base
 
 	get '/' do
-		"Hello, Grillkorv!"
-		slim(:index)
+			"Hello, Grillkorv!"
+			slim(:index)
 	end
 
 	get '/register' do
-	 slim(:register)
- end
+		 slim(:register)
+	end
+
+	post '/login' do
+		db = SQLite3::Database.new("database/chatsystem.db")
+		username = params[:username]
+		password = params[:password]
+		if password == db.execute('SELECT password FROM Users where username=?',[username])
+			session[:user_id] = user_id
+		end
+	end
+
+	post '/register' do
+		db = SQLite3::Database.new("database/chatsystem.db")
+		username = params[:username]
+		password = params[:password]
+		email = params[:email]
+		db.execute("INSERT INTO Users(username,password,email) VALUES(?,?,?)",[username,password,email])
+	#	puts "something went wrong, try again"
+	#	redirect('/register')
+		redirect('/')
+	end
 
 end
