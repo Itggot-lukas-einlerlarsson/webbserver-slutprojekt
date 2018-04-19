e91f56b4a12d5eb806f54cf1a861d028f8fde728
Itggot-lukas-einlerlarsson <Lukas.einlerlarsson@itggot.se>
2018-04-19 10:47:37 +0200

    fixed login

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 class App < Sinatra::Base
 
+	connections = []
+
 	get '/' do
 			"Hello, Grillkorv!"
 			slim(:index)
@@ -9,13 +11,32 @@ class App < Sinatra::Base
 		 slim(:register)
 	end
 
+
+	def set_error(error_message)
+		session[:error] = error_message
+	end
+
+
 	post '/login' do
 		db = SQLite3::Database.new("database/chatsystem.db")
-		username = params[:username]
-		password = params[:password]
-		if password == db.execute('SELECT password FROM Users where username=?',[username])
-			session[:user_id] = user_id
+		username = params["username"]
+		password = params["password"]
+		db_password = db.execute('SELECT password FROM Users where username=?',[username])[0][0]
+		p db_password
+		p password
+		if password == db_password
+			session[:user_id] = username
+			p 'succefully logged in'
+			redirect('/chat')
+		else
+			p 'invalid credentials'
+			redirect('/')
 		end
+	end
+
+	get '/chat' do
+		user_id = session[:user_id]
+		slim(:chat)
 	end
 
 	post '/register' do
@@ -24,8 +45,6 @@ class App < Sinatra::Base
 		password = params[:password]
 		email = params[:email]
 		db.execute("INSERT INTO Users(username,password,email) VALUES(?,?,?)",[username,password,email])
-	#	puts "something went wrong, try again"
-	#	redirect('/register')
 		redirect('/')
 	end
 
