e06c7ac43df21117de69b7e7de26a17a2ec505b4
Itggot-lukas-einlerlarsson <Lukas.einlerlarsson@itggot.se>
2018-04-26 14:20:50 +0200

    began programmin the loop and fixed some errors

--- app.rb
+++ app.rb
@@ -16,14 +16,20 @@ class App < Sinatra::Base
 		session[:error] = error_message
 	end
 
-
 	post '/login' do
 		db = SQLite3::Database.new("database/chatsystem.db")
 		username = params["username"]
 		password = params["password"]
+
+		begin
 		db_password = db.execute('SELECT password FROM Users where username=?',[username])[0][0]
+		rescue
+			p 'no such username exist'
+			redirect ('/')
+		else
 		p db_password
 		p password
+		end
 		if password == db_password
 			session[:user_id] = username
 			p 'succefully logged in'
@@ -39,10 +45,19 @@ class App < Sinatra::Base
 		slim(:chat)
 	end
 
+	post '/chat' do
+		new_subject = 200
+		time = 0
+		while time < new_subject
+			db.execute('SELECT message FROM Content')
+		end
+	end
+
 	post '/register' do
 		db = SQLite3::Database.new("database/chatsystem.db")
 		username = params[:username]
 		password = params[:password]
+#		password_digest == BCrypt::Password.new(passwor)
 		email = params[:email]
 		db.execute("INSERT INTO Users(username,password,email) VALUES(?,?,?)",[username,password,email])
 		redirect('/')
