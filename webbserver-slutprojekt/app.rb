2f1bf37efd02effd4351f586062c12c521050370
Itggot-lukas-einlerlarsson <Lukas.einlerlarsson@itggot.se>
2018-05-03 23:12:55 +0200

    update av filerna

---
+++ webbserver-slutprojekt/app.rb
@@ -0,0 +1,67 @@
+class App < Sinatra::Base
+
+	connections = []
+
+	get '/' do
+			"Hello, Grillkorv!"
+			slim(:index)
+	end
+
+	get '/register' do
+		 slim(:register)
+	end
+
+	def set_error(error_message)
+		session[:error] = error_message
+	end
+
+	post '/login' do
+		db = SQLite3::Database.new("database/chatsystem.db")
+		username = params["username"]
+		password = params["password"]
+		begin
+		db_password = db.execute('SELECT password FROM Users where username=?',[username])[0][0]
+		rescue
+			p 'no such username exist'
+			redirect ('/')
+		else
+		p db_password
+		p password
+		end
+	#	if BCrypt::Password.new(password) == db_password
+	if password == db_password
+			session[:user_id] = username
+			p 'succefully logged in'
+			redirect('/chat')
+		else
+			p 'invalid credentials'
+			redirect('/')
+		end
+	end
+
+	get '/chat' do
+		user_id = session[:user_id]
+	end
+
+	post '/chat' do
+		new_subject = 200
+		time = 0
+		messages = db.execute('SELECT Message FROM Content')
+		for message in messages
+			p message
+		end
+		slim(:chat, locals:{messages:messages})
+	end
+
+	post '/register' do
+		db = SQLite3::Database.new("database/chatsystem.db")
+		username = params[:username]
+		password = params[:password]
+#		password_digest = BCrypt::Password.create(password)
+		email = params[:email]
+#		db.execute("INSERT INTO Users(username,password,email) VALUES(?,?,?)",[username,password_digest,email])
+db.execute("INSERT INTO Users(username,password,email) VALUES(?,?,?)",[username,password,email])
+		redirect('/')
+	end
+
+end
